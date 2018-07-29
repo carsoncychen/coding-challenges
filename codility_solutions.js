0b273e737be99a1c4c8c225003fa6981553db7fa
Carson Chen <caiyongchen@live.com>
2018-07-28 21:00:35 -0400

    closure

--- codility_solutions.js
+++ codility_solutions.js
@@ -59,3 +59,27 @@ function solution(A) {
     }
     return result;
 }
+
+// ============================
+
+// practice closure and recursion
+
+printTriangle = () => {
+  let count = 1; // variable stored in closure
+  printNum = (num) => {
+    if (count <= num) {
+      let printHolder = '';
+      for(let i = 0; i < count; i++) {
+        printHolder += '#';
+      }
+      console.log(printHolder);
+      count ++;
+      printNum(num);
+    }
+  }
+  return printNum;
+}
+
+const tester = printTriangle();
+tester(10);
+
