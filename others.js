70f3868ff02f18549bc67013bea768cd1c7e5f0f
Carson Chen <carson.cy.chen@gmail.com>
2019-04-18 15:47:36 -0400

    queue and concurrency

--- others.js
+++ others.js
@@ -22,3 +22,39 @@ console.dir(tester);
 
 
 // ===============================
+
+// write a class that throttles the concurrency and responds
+// to the following interface
+class Queue {
+  constructor(obj) {
+    this.concurrency = obj.concurrency;
+    this.storage = [];
+  }
+
+  add(...jobs) {
+    for (let i = 0; i < jobs.length; i++) {
+      this.storage[this.storage.length] = jobs[i];
+    }
+  }
+
+  runJob() {
+    if (!this.storage.length) {
+      return;
+    }
+    const job = this.storage.shift();
+    job().then(() => { this.runJob() })
+  }
+
+  start() {
+    for (let i = 0; i < this.concurrency; i++) {
+      this.runJob();
+    };
+  }
+}
+
+let queue = new Queue({ concurrency: 10 })
+
+queue.add(...jobs(100));
+queue.start();
+
+// ========================
